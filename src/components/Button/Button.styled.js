6dade7bae986e5ccd6603b68ed561cbe5beb9371
Taras-Novitskyi <novitskyitaras@gmail.com>
2023-08-17 23:58:15 +0300

    fix btn

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -4,19 +4,19 @@ export const Btn = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  
+
   width: 160px;
   height: 40px;
 
-  color: #a6a9b3;
-  background-color: #21242b;
+  color: #e4e6f0;
+  background-color: ${(p) => p.theme.colors.lightBgColor};
   border: 1px solid #a6a9b3;
   border-radius: 4px;
 
   transition: all 0.25s ease-out;
 
   &:hover {
-    color: #e4e6f0;
+    color: #ffffff;
     border: 1px solid #e4e6f0;
   }
 `;
--- src/pages/MainPage/MainPage.styled.js
+++ src/pages/MainPage/MainPage.styled.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { NavLink } from "react-router-dom";
 
 export const Title = styled.div`
   margin-bottom: 40px;
